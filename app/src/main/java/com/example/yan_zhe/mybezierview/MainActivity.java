a9c04bfc23f2c3596a4eedde45e84c90057ff398
yan_zhe <yan_zhe@Ctrip.com>
2017-06-29 16:04:39 +0800

    init

---
+++ app/src/main/java/com/example/yan_zhe/mybezierview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yan_zhe.mybezierview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
